09ddf063a981c20425ef09e729d85c19b9711e92
863944536 <863944536@qq.com>
2019-03-31 22:40:10 +0800

    Product Module complite whitout test

---
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -0,0 +1,50 @@
+package com.mmall.service.impl;
+
+import com.google.common.collect.Lists;
+import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Service("iFileService")
+public class FileServiceImpl implements IFileService {
+
+    private Logger log = LoggerFactory.getLogger(FileServiceImpl.class);
+
+    public String upload(MultipartFile file,String path){
+        String fileName = file.getOriginalFilename();
+        //扩展名
+        //abc.jsp
+        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
+        String upLoadFileName = UUID.randomUUID().toString()+"."+fileExtensionName;
+        log.info("开始上传文件，上传的文件名：{},上传路径：{},新文件名：{}",fileName,path,upLoadFileName);
+        File fileDir = new File(path);
+        if(!fileDir.exists()){
+            fileDir.setReadable(true);
+            fileDir.mkdirs();
+        }
+        File targetFile = new File(path,upLoadFileName);
+        try {
+            file.transferTo(targetFile);
+            //文件已经上传成功了
+
+            //todo 将文件上传到ftp服务器上
+            FTPUtil.uploadFile(Lists.<File>newArrayList(targetFile));
+            //todo 上传完后删除upload下的文件
+            targetFile.delete();
+        } catch (IOException e) {
+            log.info("上传文件异常",e);
+            return null;
+        }
+        return targetFile.getName();
+    }
+
+
+}
