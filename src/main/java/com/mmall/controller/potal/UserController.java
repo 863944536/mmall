7e63b822db57320007970cf2cbae3c870a67c147
863944536 <863944536@qq.com>
2019-04-09 11:34:48 +0800

    commit cookie

--- src/main/java/com/mmall/controller/potal/UserController.java
+++ src/main/java/com/mmall/controller/potal/UserController.java
@@ -44,12 +44,7 @@ public class UserController {
         ServerResponse<User> response = iUserService.login(username, password);
         if(response.isSuccess()){
 //            session.setAttribute(Const.CURRENT_USER,response.getData());
-
-
             CookieUtil.writeLoginToken(httpServletResponse,session.getId());
-            CookieUtil.readLoginToken(httpServletRequest);
-            CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
-
             RedisPoolUtil.setEx(session.getId(), JsonUtil.objToString(response.getData()),Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
         }
         return response;
